09207b5d564070b2538305ecc2c743893aedbab9
Vexcompany <vexcorporation43@gmail.com>
2026-06-20 14:07:14 +0700

    Update script.js

--- public/script.js
+++ public/script.js
@@ -25,7 +25,7 @@ async function sendMessage() {
   let fullText = '';
 
   try {
-    const response = await fetch('/chat', {
+    const response = await fetch('/api/chat', {
       method: 'POST',
       headers: { 'Content-Type': 'application/json' },
       body: JSON.stringify({ prompt }),
